5597526a68607739e2f74e15055f90cae6c23283
Carla Acha <usernameexperience@gmail.com>
2025-08-22 01:28:48 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <head>
+      <body>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&family=Poppins:wght@700&family=Inter:wght@400;500&display=swap" rel="stylesheet" />
-      </head>
-      <body className="font-body antialiased">
-        {children}
-        <Toaster />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;700&family=Poppins:wght@700;800&family=Inter:wght@400;500&family=Nunito+Sans:wght@700;800&display=swap" rel="stylesheet" />
+        <div className="font-body antialiased">
+            {children}
+            <Toaster />
+        </div>
       </body>
     </html>
   );
